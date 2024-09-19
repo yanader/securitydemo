3d64bf94b4ef7debcc0619890b3e1a84743bf152
ste <mrbarrow@hotmail.co.uk>
2024-09-19 16:58:34 +0100

    feat: AuthEntryPoint

---
+++ src/main/java/com/yanader/securitydemo/jwt/AuthEntryPointJwt.java
@@ -0,0 +1,29 @@
+package com.yanader.securitydemo.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+        throws IOException, ServletException {
+        logger.error("Unauthorised error: {}", authException.getMessage());
+        System.out.println(authException);
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
