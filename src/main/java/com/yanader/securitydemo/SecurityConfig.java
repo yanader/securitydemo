ceeb7cc9606412ab5612d02fa359834dc6ab39ca
ste <mrbarrow@hotmail.co.uk>
2024-09-19 13:58:59 +0100

    feat:SecurityConfig -> Store users in H2 database

--- src/main/java/com/yanader/securitydemo/SecurityConfig.java
+++ src/main/java/com/yanader/securitydemo/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.yanader.securitydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,12 +14,18 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -47,6 +54,12 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user1, admin);
+
+        JdbcUserDetailsManager userDetailsManager
+                = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+        //        return new InMemoryUserDetailsManager(user1, admin);
     }
 }
