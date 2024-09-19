934140ad44943bc777d62be3d1f1273cb308e1d2
ste <mrbarrow@hotmail.co.uk>
2024-09-19 12:46:10 +0100

    initial commit

---
+++ src/main/java/com/yanader/securitydemo/GreetingsController.java
@@ -0,0 +1,28 @@
+package com.yanader.securitydemo;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello everyone";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "hello user";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "hello admin";
+    }
+
+
+}
