d95a27752aa0ee0f026ed61aa206476cab6c7e0b
ste <mrbarrow@hotmail.co.uk>
2024-09-21 21:35:46 +0100

    feat:profile endpoint

--- src/main/java/com/yanader/securitydemo/GreetingsController.java
+++ src/main/java/com/yanader/securitydemo/GreetingsController.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -77,5 +78,41 @@ public class GreetingsController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/profile")
+    public ResponseEntity<?> getUserProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        Map<String, Object> profile = new HashMap<>();
+        profile.put("username", userDetails.getUsername());
+        profile.put("roles", userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()));
+        profile.put("message", "This is user-specific content from backend");
+
+        return ResponseEntity.ok(profile);
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
